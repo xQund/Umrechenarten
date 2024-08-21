b69a9f18e73e57b7ce370995e8d56f285bf5df12
xQund <paul.kramer1@hotmail.com>
2024-08-21 19:00:53 +0200

    Version 3
    2 Rechnungen ausgetauscht und Rechnen + Ausgabe vereinfacht.
    Schlussfrage zum erneut Starten und Ausgabe bei falscher Nummer hinzugefügt.

--- Umrechenarten.py
+++ Umrechenarten.py
@@ -1,84 +1,69 @@
-#Variablen
-ausw1 = 0
-ausw2 = 0
-
-#Auswählen
-while ausw1 != 1 and ausw1 != 2 and ausw1 != 3 and ausw1 != 4 and ausw1 != 5:
-    ausw1 = int(input("1: m <->km \nk2: s <-> h \n3: m/s <-> km/h \n4: m/s <-> m <-> s\n5: km/h <-> km <-> h\nNummer eingeben:" ))
-
-#Zahlen einfügen + Rechnen + Ergebnisse
-if ausw1 == 1:
-    while ausw2 != 1 and ausw2 != 2:
-        ausw2 = int(input("1: m -> km \n2: km -> m \nNummer eingeben:"))
-    if ausw2 == 1:
-        zahl1 = float(input("Meter eingeben:"))
-        ergebniss = zahl1 / 1000
-        print(f"{zahl1}m = {ergebniss}km")
-    if ausw2 == 2:
-        zahl1 = float(input("Kilometer eingeben:"))
-        ergebniss = zahl1 * 1000
-        print(f"{zahl1}km = {ergebniss}m")
+#Nach dem Umrechnen Wiederholen
+while ausw3 != 2:
     
-elif ausw1 == 2:
-    while ausw2 != 1 and ausw2 != 2:
-        ausw2 = int(input("1: s -> h \n2: h -> s \nNummer eingeben:"))
-    if ausw2 == 1:
-        zahl1 = float(input("Sekunden eingeben:"))
-        ergebniss = zahl1 / 3600
-        print(f"{zahl1}s = {ergebniss}h")
-    if ausw2 == 2:
-        zahl1 = float(input("Stunden eingeben:"))
-        ergebniss = zahl1 * 3600
-        print(f"{zahl1}h = {ergebniss}s")
-        
-elif ausw1 == 3:
-    while ausw2 != 1 and ausw2 != 2:
-        ausw2 = int(input("1: m/s -> km/h \n2: km/h -> m/s \nNummer eingeben:"))
-    if ausw2 == 1:
-        zahl1 = float(input("Meter pro Sekunde eingeben:"))
-        ergebniss = zahl1 * 3.6
-        print(f"{zahl1}m/s = {ergebniss}km/h")
-    if ausw2 == 2:
-        zahl1 = float(input("Kilometer pro Stunde eingeben:"))
-        ergebniss = zahl1 / 3.6
-        print(f"{zahl1}km/h = {ergebniss}m/s")
-        
-elif ausw1 == 4:
-    while ausw2 != 1 and ausw2 != 2 and ausw2 != 3:
-        ausw2 = int(input("1: m * s -> m/s \n2: m/s / s -> m \n3: m/s / m -> s \nNummer eingeben:"))
-    if ausw2 == 1:
-        zahl1 = float(input("Meter eingeben:"))
-        zahl2 = float(input("Sekunde eingeben:"))
-        ergebniss = zahl1 * zahl2
-        print(f"{zahl1}m * {zahl2}s = {ergebniss}m/s")
-    if ausw2 == 2:
-        zahl1 = float(input("Meter pro Sekunde eingeben:"))
-        zahl2 = float(input("Sekunde eingeben:"))
-        ergebniss = zahl1 / zahl2
-        print(f"{zahl1}m/s / {zahl2}s = {ergebniss}m")
-    if ausw2 == 3:
-        zahl1 = float(input("Meter pro Sekunde eingeben:"))
-        zahl2 = float(input("Meter eingeben:"))
-        ergebniss = zahl1 / zahl2
-        print(f"{zahl1}m/s / {zahl2}m = {ergebniss}s")
-        
-elif ausw1 == 5:
-    while ausw2 != 1 and ausw2 != 2 and ausw2 != 3:
-        ausw2 = int(input("1: km * h -> km/h \n2: km/h / h -> km \n3: km/h / km -> h \nNummer eingeben:"))
-    if ausw2 == 1:
-        zahl1 = float(input("Kilometer eingeben:"))
-        zahl2 = float(input("Stunde eingeben:"))
-        ergebniss = zahl1 * zahl2
-        print(f"{zahl1}km * {zahl2}h = {ergebniss}km/h")
-    if ausw2 == 2:
-        zahl1 = float(input("Kilometer pro Stunde eingeben:"))
-        zahl2 = float(input("Stunde eingeben:"))
-        ergebniss = zahl1 / zahl2
-        print(f"{zahl1}km/h / {zahl2}h = {ergebniss}km")
-    if ausw2 == 3:
-        zahl1 = float(input("Kilometer pro Stunde eingeben:"))
-        zahl2 = float(input("Kilometer eingeben:"))
-        ergebniss = zahl1 / zahl2
-        print(f"{zahl1}km/h / {zahl2}km = {ergebniss}h")
+#Variablen
+    ausw1 = 0
+    ausw2 = 0
+    ausw3 = 0
+    num1 = 0
+    
+#Auswahl der Umrechenart    
+    while ausw1 != 1 and ausw1 != 2 and ausw1 != 3 and ausw1 != 4 and ausw1 != 5:
+        print("___________________")
+        if num1 != 0:
+            print("Ungülltige Nummer, bitte erneut eingeben!")
+        ausw1 = int(input("1: m <->km \n2: cm <-> Inch \n3: s <-> h \n4: sm <-> km \n5: m/s <-> km/h \nNummer eingeben:" ))
+        num1 = num1 + 1
 
-print("Ende")
+#Strings und feste Integer benennen
+    if ausw1 == 1:
+        formel1 = "m"
+        formel2 = "km"
+        zahl2 = 1000
+    elif ausw1 == 2:
+        formel1 = "cm"
+        formel2 = "Inch"
+        zahl2 = 2.54
+    elif ausw1 == 3:
+        formel1 = "s"
+        formel2 = "h"
+        zahl2 = 3600
+    elif ausw1 == 4:
+        formel1 = "sm"
+        formel2 = "km"
+        zahl2 = 1.852
+    elif ausw1 == 5:
+        formel1 = "m/s"
+        formel2 = "km/h"
+        zahl2 = 3.6
+   
+#Auswahl der Einheit
+    num1 = 0
+    while ausw2 != 1 and ausw2 != 2:
+        print("___________________")
+        if num1 != 0:
+            print("Ungülltige Nummer, bitte erneut eingeben!")
+        ausw2 = int(input(f"1: {formel1} -> {formel2} \n2: {formel2} -> {formel1} \nNummer eingeben:"))
+        num1 = num1 + 1
+    
+#Eingabe der Zahl + Umrechnen
+    if ausw2 == 1:
+        zahl1 = float(input(f"___________________\nBitte {formel1} eingeben:"))
+        ergebniss = zahl1 / zahl2
+        print(f"{zahl1}{formel1} = {ergebniss}{formel2}")
+    if ausw2 == 2:
+        zahl1 = float(input(f"___________________\nBitte {formel2} eingeben:"))
+        ergebniss = zahl1 * zahl2
+        print(f"{zahl1}{formel2} = {ergebniss}{formel1}")
+
+#Auswahl zum Verlassen
+    num1 = 0
+    while ausw3 != 1 and ausw3 != 2:
+        print("___________________")
+        if num1 != 0:
+            print("Ungülltige Nummer, bitte erneut eingeben!")
+        ausw3 = int(input("1: Nochmal!!:) \n2: Verlassen \nNummer eingeben:"))
+        num1 = num1 + 1
+
+#Ende
+print("___________________\nEnde")
