098c21beb8d0321947077e88153ed926149133aa
xQund <paul.kramer1@hotmail.com>
2024-08-22 12:20:51 +0200

    Version 4
    Definitionen in das Programm eingepflegt.
    das Verlassen des Programmes in die erste Auswahl und in der zweiten Auswahl eine Zurück Option  reingepackt.
    Die Auswahl der Formelzeichen und des Umrechnungsfaktors mit Tupel verkürzt.
    Auswahlen als String umgeändert um Programmfehler zu vermeiden.
    Kommentare für die Übersichtlichkeit hinzugefügt.

--- Umrechenarten.py
+++ Umrechenarten.py
@@ -1,69 +1,103 @@
-#Nach dem Umrechnen Wiederholen
-while ausw3 != 2:
-    
-#Variablen
-    ausw1 = 0
-    ausw2 = 0
-    ausw3 = 0
-    num1 = 0
-    
-#Auswahl der Umrechenart    
-    while ausw1 != 1 and ausw1 != 2 and ausw1 != 3 and ausw1 != 4 and ausw1 != 5:
-        print("___________________")
-        if num1 != 0:
-            print("Ungülltige Nummer, bitte erneut eingeben!")
-        ausw1 = int(input("1: m <->km \n2: cm <-> Inch \n3: s <-> h \n4: sm <-> km \n5: m/s <-> km/h \nNummer eingeben:" ))
-        num1 = num1 + 1
+#Kommentare immer über oder Neben dem jeweiligen Schritt
 
-#Strings und feste Integer benennen
-    if ausw1 == 1:
-        formel1 = "m"
-        formel2 = "km"
-        zahl2 = 1000
-    elif ausw1 == 2:
-        formel1 = "cm"
-        formel2 = "Inch"
-        zahl2 = 2.54
-    elif ausw1 == 3:
-        formel1 = "s"
-        formel2 = "h"
-        zahl2 = 3600
-    elif ausw1 == 4:
-        formel1 = "sm"
-        formel2 = "km"
-        zahl2 = 1.852
-    elif ausw1 == 5:
-        formel1 = "m/s"
-        formel2 = "km/h"
-        zahl2 = 3.6
-   
+#Variablen bestimmen
+formelzeichen1 = "m", "cm", "s", "sm", "km/h", "km", "Inch", "h", "km", "m/s"
+umrechenfaktoren1 = 1000, 2.54, 3600, 1.852, 3.6
+#________________________________________________________________________________________________________________
+#Auswahl der Umrechenart
+def auswahl1(temp_ausw1 = 0):
+    num1 = False
+    #While Schleife wiederholt die Eingabe, bis eine Zahl von 1-6 eingegeben wurde
+    while temp_ausw1 !="1" and temp_ausw1 !="2" and temp_ausw1 !="3" and temp_ausw1 !="4" and temp_ausw1 !="5" and temp_ausw1 !="6":
+        print("___________________")
+        #Abfrage ob diese Schleife schon einmal durchlaufen worden ist
+        if num1 == True:      
+            print("Ungülltige Eingabe, bitte Nummer von 1-5 eingeben!")
+        else:
+            num1 = True
+        #Eingabe zur Auswahl der Umrechenarten
+        temp_ausw1 = input("1: m <-> km \
+                          \n2: cm <-> Inch \
+                          \n3: s <-> h \
+                          \n4: sm <-> km \
+                          \n5: m/s <-> km/h \
+                          \n6: Verlassen \
+                          \nNummer eingeben:")
+        
+    #Zurückgabe der eingegebenen Nummer (1-6)
+    return temp_ausw1          
+#________________________________________________________________________________________________________________
 #Auswahl der Einheit
-    num1 = 0
-    while ausw2 != 1 and ausw2 != 2:
+def auswahl2(temp_ausw2 = 0):
+    num1 = False
+    #While Schleife wiederholt die Eingabe, bis eine Zahl von 1-3 eingegeben wurde
+    while temp_ausw2 !="1" and temp_ausw2 !="2" and temp_ausw2 !="3":
         print("___________________")
-        if num1 != 0:
-            print("Ungülltige Nummer, bitte erneut eingeben!")
-        ausw2 = int(input(f"1: {formel1} -> {formel2} \n2: {formel2} -> {formel1} \nNummer eingeben:"))
-        num1 = num1 + 1
-    
-#Eingabe der Zahl + Umrechnen
-    if ausw2 == 1:
-        zahl1 = float(input(f"___________________\nBitte {formel1} eingeben:"))
+        #Abfrage ob diese Schleife schon einmal durchlaufen worden ist
+        if num1 == True:       
+            print("Ungülltige Eingabe, bitte Nummer von 1-2 eingeben!")
+        else:
+            num1 = True
+        
+        temp_ausw2 = input(f"1: {formel1} -> {formel2} \
+                           \n2: {formel2} -> {formel1} \
+                           \n3: Zurück \
+                           \nNummer eingeben:")
+        
+    #Zurückgabe der eingegebenen Nummer (1-3)
+    return temp_ausw2         
+#________________________________________________________________________________________________________________    
+#Eingabe und Umrechnen der gewünschten Größe
+def rechnen(zahl1 = 0, eing = 0):
+    num1 = False
+    #While Schleife wiederholt die Eingabe, bis eine Zahl eingegeben wurde
+    while zahl1 == 0:
+        print("___________________")
+        #Abfrage ob diese Schleife schon einmal durchlaufen worden ist
+        if num1 == True:       
+            print("Ungülltige Eingabe, bitte Nummer nur Zahlen und maximal einen . eingeben!")
+        else:
+            num1 = True
+        #Eingabe der Umzurechnenden Zahl
+        eing1 = str(input(f"Bitte {formel1} eingeben:"))
+        #Eingabe Überprufen
+        try:
+            zahl1 = float(eing1)
+        except:
+            num1 = True
+            
+    #Rechnen je nach Umstellrichtung
+    if ausw2 == "1":                                     
         ergebniss = zahl1 / zahl2
-        print(f"{zahl1}{formel1} = {ergebniss}{formel2}")
-    if ausw2 == 2:
-        zahl1 = float(input(f"___________________\nBitte {formel2} eingeben:"))
+    elif ausw2 == "2":
         ergebniss = zahl1 * zahl2
-        print(f"{zahl1}{formel2} = {ergebniss}{formel1}")
-
-#Auswahl zum Verlassen
-    num1 = 0
-    while ausw3 != 1 and ausw3 != 2:
-        print("___________________")
-        if num1 != 0:
-            print("Ungülltige Nummer, bitte erneut eingeben!")
-        ausw3 = int(input("1: Nochmal!!:) \n2: Verlassen \nNummer eingeben:"))
-        num1 = num1 + 1
-
-#Ende
-print("___________________\nEnde")
+    #Ausgabe des Umgerechneten Ergebnisses        
+    print(f"{zahl1}{formel1} = {ergebniss}{formel2}")
+    #Pausieren des Programmes um das Ergebnis zu Presentieren
+    ausw3 = input("Drücke Enter um Fortzufahren")
+#________________________________________________________________________________________________________________    
+#Ausführung des Programms als Schleife
+while True:
+    #"auswahl1" Aufrufen und in ausw2 als Integer schreiben, zum Rechnen
+    ausw1 = int(auswahl1())                   
+    #Schleife Beenden, wenn die Nummer 6 "Verlassen" ausgewählt wurde
+    if ausw1 == 6:                            
+        print("\nProgramm Beendet")
+        break
+   
+    #Formelzeichen als String-Variable und Umrechenfaktor als Integer-Variable bestimmen
+    formel1 = formelzeichen1[ausw1 - 1]       #Erstes Formelzeichen
+    formel2 = formelzeichen1[ausw1 + 4]       #Zweites Formelzeichen
+    zahl2 = umrechenfaktoren1[ausw1 - 1]      #Umrechenfaktor
+    
+    #"auswahl2" Aufrufen und in ausw2 schreiben   
+    ausw2 = auswahl2()
+    #Drehen der Formelzeichen
+    if ausw2 == "2":                          
+        formel1, formel2 = formel2, formel1
+    #Schleife von vorne Beginnen, wenn die Nummer 3 "Zurück" ausgewählt wurde
+    elif ausw2 == "3":                        
+        continue
+    
+    #"rechnen" Aufrufen
+    rechnen()
